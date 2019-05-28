052aa5fd3d6c38f401752ced60cbc2e0574f10ba
annabengel <annambengel@gmail.com>
2019-05-28 11:46:33 -0400

    initial project creation

---
+++ PersonManagerApp/src/main/java/com/pm/PersonManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonManagerAppApplication.class, args);
+	}
+
+}
